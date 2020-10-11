e705ad1e6b0cb55c9fcb4aab21a89eb670aa3ab9
alekyalokasani <59576868+alekyalokasani@users.noreply.github.com>
2020-10-11 05:56:02 -0400

    Update script.js
    Created Java Validations for the fields Address, to match the passwords, and to generate a message.

--- Lab-1/script.js
+++ Lab-1/script.js
@@ -2,23 +2,47 @@ function formValidation()
 {
        var ucountry = document.registration.country;
        var ustate = document.registration.State;
+       var uadd = document.registration.Address;
        var uemail = document.getElementById("email").value;
        var uRemail = document.getElementById("ReTypeEmail").value;
        var umsex = document.registration.msex;
        var ufsex = document.registration.fsex;
+       var uPass = document.getElementById("password1").value;
+       var uCPass = document.getElementById("password2").value;
+       var uMess = document.registration.About.value;
        {
         if(countryselect(ucountry))
         {
         if(Stateselect(ustate))
         {
+            if(alphanumeric(uadd))
+    {
             if(validsex(umsex,ufsex))
+    {
+        if(validpass(uPass,uCPass))
     {
         if(validemail(uemail,uRemail))
     {
+        if(validmess(uMess)){
+        }
+    }
+}
        }
     }
 }
         }}}
+        function validmess(uMess){
+            if(uMess == "" )
+            {
+              alert("Type your Message");
+              return false;
+            }
+            else{
+              alert('Form Succesfully Submitted');
+              window.location.reload()
+              return true;
+            }
+          }
         function validemail(uemail,uRemail)
 {
     if(uemail != uRemail) 
@@ -58,6 +82,35 @@ return false;
 else
 {
 return true;
+}
+}
+function validpass(uPass,uCPass)
+  {
+
+  
+  if (uPass != uCPass) {
+    alert("Passwords do not match.");
+    return false;
+  }
+
+return true;
+}
+
+
+  
+
+  function alphanumeric(uadd)
+{ 
+var letters = /^[0-9a-zA-Z]+$/;
+if(uadd.value.match(letters))
+{
+return true;
+}
+else
+{
+alert('User address must have alphanumeric characters only');
+uadd.focus();
+return false;
 }
 }
 function validsex(umsex,ufsex)
